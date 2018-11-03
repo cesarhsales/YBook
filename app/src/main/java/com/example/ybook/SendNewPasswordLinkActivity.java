c76fe42170fe2259c5369095b4bdf59ff48e0dee
David Souza <david.jsouzaa@gmail.com>
2018-11-03 17:37:30 -0300

    I created the inicial XML files following the mocks

---
+++ app/src/main/java/com/example/ybook/SendNewPasswordLinkActivity.java
@@ -0,0 +1,13 @@
+package com.example.ybook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendNewPasswordLinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_new_password_link);
+    }
+}
